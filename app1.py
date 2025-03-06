3f8797357e5a5d28691c522e43bbf950f1fcaa2c
Kunozga Mlowoka <46397966+kunozga265@users.noreply.github.com>
2025-03-06 20:30:37 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import time
 from langchain_community.document_loaders import PyPDFLoader
@@ -9,10 +13,6 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
-
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from dotenv import load_dotenv
 load_dotenv()
